9c7598d1a29acc666df50fdcd3c804e25bba8b47
Lucas Rennó Kallás <lucasrk@unifei.edu.br>
2020-09-05 15:06:57 -0300

    first commit

---
+++ web/src/pages/Landing/Landing.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import './styles.css'
+
+function Landing() {
+  return (
+    <div className="page-landing">
+      <h1>test</h1>
+    </div>
+  );
+}
+
+export default Landing;
