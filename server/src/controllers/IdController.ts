8a1b66bad4190c8937519c8c627ffb0a9cca6b92
Lucas Rennó Kallás <lucasrk@unifei.edu.br>
2020-09-05 20:15:57 -0300

    server create index routes

--- server/src/controllers/IdController.ts
+++ server/src/controllers/IdController.ts
@@ -5,29 +5,32 @@ export default class IdController{
 
     //get the id with the files or return undefined
     async index(request: Request, response: Response){
-        const {template} = request.query;
+        const {id} = request.query;
 
-        return response.json({"a": "b"});
+        console.log(id)
+        const selectedFiles = db('files')
+            .select('*')
+            .where('files.id_url', '=', id as string)
+
+        return response.json(selectedFiles);
     }
 
     //create an id with the files
     async create(request: Request, response: Response){
         const {
-            template,
-            top,
-            right,
-            bottom,
-            left
+            id,
+            file
         } = request.body;
     
         const trx = await db.transaction();
         try{
-            await trx('template_margins').insert({
-                template,
-                top,
-                right,
-                bottom,
-                left
+            await trx('id').insert({
+                id
+            });
+            
+            await trx('files').insert({
+                id_url: id,
+                file
             });
             
             await trx.commit();
@@ -37,7 +40,7 @@ export default class IdController{
             await trx.rollback();
     
             return response.status(400).json({
-                error: "Unexpected error while creating the template margin."
+                error: "Unexpected error while creating the id url."
             })
         }
     }
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -2,9 +2,9 @@ import express from 'express';
 import IdController from './controllers/IdController';
 
 const routes = express.Router();
-// const idController = new IdController();
+const idController = new IdController();
 
-// routes.post('/id', idController.create);
-// routes.post('/id', idController.index);
+routes.post('/', idController.create);
+routes.get('/', idController.index);
 
 export default routes;
