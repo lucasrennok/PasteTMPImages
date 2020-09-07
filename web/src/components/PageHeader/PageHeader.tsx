d4c85697605960f2046d241d9b23ebbf25856022
Lucas Rennó Kallás <lucasrk@unifei.edu.br>
2020-09-06 22:28:52 -0300

    warnings removed

--- web/src/components/PageHeader/PageHeader.tsx
+++ web/src/components/PageHeader/PageHeader.tsx
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
 
 import './styles.css'
-import api from '../../services/api';
-import { ifError } from 'assert';
+import { Redirect } from 'react-router-dom';
 
 interface PageHeaderProps{
     idPaste: string;
@@ -10,32 +9,31 @@ interface PageHeaderProps{
 
 const PageHeader: React.FC<PageHeaderProps>= (pageHeaderProps) => {    
     const [idPaste, setIdPaste] = useState(pageHeaderProps.idPaste);
+    const [redirect, setRedirect] = useState((<div></div>));
 
     function handleSearchFiles(){
         if(idPaste.length!==10){
             alert('Id is wrong.')
         }else{
-            api.get('?id='+encodeURIComponent(idPaste)).then(response => {
-                if(response.data.length===0){
-                    alert('Id is wrong.')
-                }else{
-                    //Redirect to filepage
-                    console.log(response.data)
-                }
-            })
+            const routeToGo = "/paste/"+idPaste;
+            if(window.location.pathname.substr(0,7)==='/paste/'){
+                window.location.pathname = routeToGo;
+            }
+            setRedirect(<Redirect to={routeToGo} />);
         }
     }
 
     return (
         <div className="page-header">
             <div className="headerText">
-                <h1>Hey! Drop your your file</h1>
+                <h1>Hey! Drop your file</h1>
                 <h3>Copy-Paste anything you want, then get it from anywhere</h3>
             </div>
             <div className="inputBox">
                 <label>File Id</label>
                 <input type="text" defaultValue={idPaste} onChange={e => setIdPaste(e.target.value)}></input>
                 <button type="button" onClick={handleSearchFiles}>Search</button>
+                {redirect}
             </div>
         </div>
     );
